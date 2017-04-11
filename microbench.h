7a801f494ebbb9ad4966c0afe6b20316bcbd896d
Ziqi Wang <675973885@qq.com>
2017-04-11 18:01:31 +0000

    Make it compile

--- microbench.h
+++ microbench.h
@@ -11,28 +11,13 @@
 
 #include "allocatortracker.h"
 
-//#include "btreeIndex.h"
-//#include "artIndex.h"
 #include "index.h"
 
-//#include <map>
-//#include "stx/btree_map.h"
-//#include "stx/btree.h"
-//#include "stx-compact/btree_map.h"
-//#include "stx-compact/btree.h"
-//#include "stx-compress/btree_map.h"
-//#include "stx-compress/btree.h"
-
-//#define INIT_LIMIT 1001
-//#define LIMIT 1000000
 #define INIT_LIMIT 50000000
 #define LIMIT 10000000
 
-#define VALUES_PER_KEY 10
-
 //#define PAPI_IPC 1
 #define PAPI_CACHE 1
-
 
 //==============================================================
 inline double get_now() {

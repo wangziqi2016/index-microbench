44642af693fc342d3821d801f3ef667131581111
Ziqi Wang <675973885@qq.com>
2017-05-07 17:08:38 -0400

    Adding enum constants for workload and keytype

--- util.h
+++ util.h
@@ -24,6 +24,20 @@ enum {
   OP_SCAN,
 };
 
+// These are YCSB workloads
+enum {
+  WORKLOAD_A,
+  WORKLOAD_C,
+  WORKLOAD_E,
+};
+
+// These are key types we use for running the benchmark
+enum {
+  RAND_KEY,
+  MONO_KEY,
+  EMAIL_KEY,
+}
+
 //==============================================================
 // GET INSTANCE
 //==============================================================

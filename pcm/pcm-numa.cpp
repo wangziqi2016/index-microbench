706019cfb63dbe1f41f512d2390f93a973bf05c0
Ziqi Wang <675973885@qq.com>
2017-05-14 07:59:13 +0000

    Changing pcm-numa.cpp to measure NUMA performance

--- pcm/pcm-numa.cpp
+++ pcm/pcm-numa.cpp
@@ -40,132 +40,33 @@
 #endif
 
 #include <vector>
-#define PCM_DELAY_DEFAULT 1.0       // in seconds
-#define PCM_DELAY_MIN 0.015         // 15 milliseconds is practical on most modern CPUs
-#define PCM_CALIBRATION_INTERVAL 50 // calibrate clock only every 50th iteration
 
 using namespace std;
 
-void print_usage(const string progname)
-{
-    cerr << endl << " Usage: " << endl << " " << progname
-         << " --help | [delay] [options] [-- external_program [external_program_options]]" << endl;
-    cerr << "   <delay>                           => time interval to sample performance counters." << endl;
-    cerr << "                                        If not specified, or 0, with external program given" << endl;
-    cerr << "                                        will read counters only after external program finishes" << endl;
-    cerr << " Supported <options> are: " << endl;
-    cerr << "  -h    | --help  | /h               => print this help and exit" << endl;
-    cerr << "  -csv[=file.csv] | /csv[=file.csv]  => output compact CSV format to screen or" << endl
-         << "                                        to a file, in case filename is provided" << endl;
-    cerr << " Examples:" << endl;
-    cerr << "  " << progname << " 1                  => print counters every second without core and socket output" << endl;
-    cerr << "  " << progname << " 0.5 -csv=test.log  => twice a second save counter values to test.log in CSV format" << endl;
-    cerr << "  " << progname << " /csv 5 2>/dev/null => one sample every 5 seconds, and discard all diagnostic output" << endl;
-    cerr << endl;
-}
-
 template <class StateType>
-void print_stats(const StateType & BeforeState, const StateType & AfterState, bool csv)
-{
+void print_stats(const StateType & BeforeState, const StateType & AfterState) {
     uint64 cycles = getCycles(BeforeState, AfterState);
     uint64 instr = getInstructionsRetired(BeforeState, AfterState);
 
-    if (csv)
-    {
-        cout << double(instr) / double(cycles) << ",";
-        cout << instr << ",";
-        cout << cycles << ",";
-    }
-    else
-    {
-        cout << double(instr) / double(cycles) << "       ";
-        cout << unit_format(instr) << "     ";
-        cout << unit_format(cycles) << "      ";
-    }
+    cout << double(instr) / double(cycles) << "       ";
+    cout << unit_format(instr) << "     ";
+    cout << unit_format(cycles) << "      ";
 
-    for (int i = 0; i < 2; ++i)
-        if (!csv)
-            cout << unit_format(getNumberOfCustomEvents(i, BeforeState, AfterState)) << "              ";
-        else
-            cout << getNumberOfCustomEvents(i, BeforeState, AfterState) << ",";
+    for (int i = 0; i < 2; ++i) {
+      cout << unit_format(getNumberOfCustomEvents(i, BeforeState, AfterState)) << "              ";
+    }
 
     cout << "\n";
 }
 
+static uint32_t ncores;
+static uint64 BeforeTime_NUMA = 0, AfterTime_NUMA = 0;
+static SystemCounterState SysBeforeState_NUMA, SysAfterState_NUMA;
+static std::vector<CoreCounterState> BeforeState_NUMA, AfterState_NUMA;
+static std::vector<SocketCounterState> DummySocketStates_NUMA;
 
-int main(int argc, char * argv[])
-{
-    set_signal_handlers();
-
-#ifdef PCM_FORCE_SILENT
-    null_stream nullStream1, nullStream2;
-    std::cout.rdbuf(&nullStream1);
-    std::cerr.rdbuf(&nullStream2);
-#endif
-
-    cerr << endl;
-    cerr << " Processor Counter Monitor: NUMA monitoring utility " << endl;
-    cerr << endl;
-
-    double delay = -1.0;
-    char * sysCmd = NULL;
-    char ** sysArgv = NULL;
-    bool csv = false;
-    long diff_usec = 0;                            // deviation of clock is useconds between measurements
-    int calibrated = PCM_CALIBRATION_INTERVAL - 2; // keeps track is the clock calibration needed
-    string program = string(argv[0]);
-
-    PCM * m = PCM::getInstance();
-
-    if (argc > 1) do
-        {
-            argv++;
-            argc--;
-            if (strncmp(*argv, "--help", 6) == 0 ||
-                strncmp(*argv, "-h", 2) == 0 ||
-                strncmp(*argv, "/h", 2) == 0)
-            {
-                print_usage(program);
-                exit(EXIT_FAILURE);
-            }
-            else if (strncmp(*argv, "-csv", 4) == 0 ||
-                     strncmp(*argv, "/csv", 4) == 0)
-            {
-                csv = true;
-                string cmd = string(*argv);
-                size_t found = cmd.find('=', 4);
-                if (found != string::npos) {
-                    string filename = cmd.substr(found + 1);
-                    if (!filename.empty()) {
-                        m->setOutput(filename);
-                    }
-                }
-                continue;
-            }
-            else if (strncmp(*argv, "--", 2) == 0)
-            {
-                argv++;
-                sysCmd = *argv;
-                sysArgv = argv;
-                break;
-            }
-            else
-            {
-                // any other options positional that is a floating point number is treated as <delay>,
-                // while the other options are ignored with a warning issues to stderr
-                double delay_input;
-                std::istringstream is_str_stream(*argv);
-                is_str_stream >> noskipws >> delay_input;
-                if (is_str_stream.eof() && !is_str_stream.fail()) {
-                    delay = delay_input;
-                } else {
-                    cerr << "WARNING: unknown command-line option: \"" << *argv << "\". Ignoring it." << endl;
-                    print_usage(program);
-                    exit(EXIT_FAILURE);
-                }
-                continue;
-            }
-        } while (argc > 1); // end of command line partsing loop
+void StartNUMAMonitor() {
+    PCM *m = PCM::getInstance();
 
     EventSelectRegister def_event_select_reg;
     def_event_select_reg.value = 0;
@@ -199,6 +100,7 @@ int main(int argc, char * argv[])
         cerr << "pcm-numa tool does not support your processor currently." << endl;
         exit(EXIT_FAILURE);
     }
+
     EventSelectRegister regs[4];
     conf.gpCounterCfg = regs;
     for (int i = 0; i < 4; ++i)
@@ -235,112 +137,32 @@ int main(int argc, char * argv[])
 
     cerr << "\nDetected " << m->getCPUBrandString() << " \"Intel(r) microarchitecture codename " << m->getUArchCodename() << "\"" << endl;
 
-    uint64 BeforeTime = 0, AfterTime = 0;
-    SystemCounterState SysBeforeState, SysAfterState;
-    const uint32 ncores = m->getNumCores();
-    std::vector<CoreCounterState> BeforeState, AfterState;
-    std::vector<SocketCounterState> DummySocketStates;
+    ncores = m->getNumCores();
+    m->setBlocked(false);
 
-    if ((sysCmd != NULL) && (delay <= 0.0)) {
-        // in case external command is provided in command line, and
-        // delay either not provided (-1) or is zero
-        m->setBlocked(true);
-    } else {
-        m->setBlocked(false);
+    BeforeTime_NUMA = m->getTickCount();
+    m->getAllCounterStates(SysBeforeState_NUMA, DummySocketStates_NUMA, BeforeState_NUMA);
+
+    return;
+}
+
+void EndNUMAMonitor() {
+    AfterTime = m->getTickCount();
+    m->getAllCounterStates(SysAfterState_NUMA, DummySocketStates_NUMA, AfterState_NUMA);
+
+    cout << "Core | IPC  | Instructions | Cycles  |  Local DRAM accesses | Remote DRAM Accesses \n";
+
+    for (uint32 i = 0; i < ncores; ++i) {
+      cout << " " << setw(3) << i << "   " << setw(2);
+      print_stats(BeforeState_NUMA[i], AfterState_NUMA[i]);
     }
 
-    if (csv) {
-        if (delay <= 0.0) delay = PCM_DELAY_DEFAULT;
-    } else {
-        // for non-CSV mode delay < 1.0 does not make a lot of practical sense:
-        // hard to read from the screen, or
-        // in case delay is not provided in command line => set default
-        if (((delay < 1.0) && (delay > 0.0)) || (delay <= 0.0)) delay = PCM_DELAY_DEFAULT;
-    }
 
-    cerr << "Update every " << delay << " seconds" << endl;
+    cout << "-------------------------------------------------------------------------------------------------------------------\n";
+    cout << "   *   ";
 
-    std::cout.precision(2);
-    std::cout << std::fixed;
+    print_stats(SysBeforeState_NUMA, SysAfterState_NUMA);
+    std::cout << std::endl;
 
-    BeforeTime = m->getTickCount();
-    m->getAllCounterStates(SysBeforeState, DummySocketStates, BeforeState);
-
-    if (sysCmd != NULL) {
-        MySystem(sysCmd, sysArgv);
-    }
-
-    while (1)
-    {
-        if (!csv) cout << std::flush;
-        int delay_ms = int(delay * 1000);
-        int calibrated_delay_ms = delay_ms;
-#ifdef _MSC_VER
-        // compensate slow Windows console output
-        if (AfterTime) delay_ms -= (int)(m->getTickCount() - BeforeTime);
-        if (delay_ms < 0) delay_ms = 0;
-#else
-        // compensation of delay on Linux/UNIX
-        // to make the samling interval as monotone as possible
-        struct timeval start_ts, end_ts;
-        if (calibrated == 0) {
-            gettimeofday(&end_ts, NULL);
-            diff_usec = (end_ts.tv_sec - start_ts.tv_sec) * 1000000.0 + (end_ts.tv_usec - start_ts.tv_usec);
-            calibrated_delay_ms = delay_ms - diff_usec / 1000.0;
-        }
-#endif
-
-        MySleepMs(calibrated_delay_ms);
-
-#ifndef _MSC_VER
-        calibrated = (calibrated + 1) % PCM_CALIBRATION_INTERVAL;
-        if (calibrated == 0) {
-            gettimeofday(&start_ts, NULL);
-        }
-#endif
-        AfterTime = m->getTickCount();
-        m->getAllCounterStates(SysAfterState, DummySocketStates, AfterState);
-
-        cout << "Time elapsed: " << dec << fixed << AfterTime - BeforeTime << " ms\n";
-        //cout << "Called sleep function for "<<dec<<fixed<<delay_ms<<" ms\n";
-
-        if (csv)
-            cout << "Core,IPC,Instructions,Cycles,Local DRAM accesses,Remote DRAM accesses \n";
-        else
-            cout << "Core | IPC  | Instructions | Cycles  |  Local DRAM accesses | Remote DRAM Accesses \n";
-
-        for (uint32 i = 0; i < ncores; ++i)
-        {
-            if (csv)
-                cout << i << ",";
-            else
-                cout << " " << setw(3) << i << "   " << setw(2);
-
-            print_stats(BeforeState[i], AfterState[i], csv);
-        }
-
-
-        if (csv)
-            cout << "*,";
-        else
-        {
-            cout << "-------------------------------------------------------------------------------------------------------------------\n";
-            cout << "   *   ";
-        }
-
-        print_stats(SysBeforeState, SysAfterState, csv);
-
-        std::cout << std::endl;
-
-        swap(BeforeTime, AfterTime);
-        swap(BeforeState, AfterState);
-        swap(SysBeforeState, SysAfterState);
-
-        if (m->isBlocked()) {
-            // in case PCM was blocked after spawning child application: break monitoring loop here
-            break;
-        }
-    }
-
-    exit(EXIT_SUCCESS);
+    return;
 }

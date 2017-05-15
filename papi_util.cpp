a2312b53810d93f3fd00ad6c08a0d5dbb700474d
Ziqi Wang <675973885@qq.com>
2017-05-15 01:50:30 +0000

    Adding inst counter

--- papi_util.cpp
+++ papi_util.cpp
@@ -1,13 +1,48 @@
 
 #include "microbench.h"
 
-static constexpr int PAPI_CACHE_EVENT_COUNT = 4;
+static constexpr int PAPI_CACHE_EVENT_COUNT = 3;
+static constexpr int PAPI_INST_EVENT_COUNT = 6;
+
+void StartInstMonitor() {
+  int events[PAPI_INST_EVENT_COUNT] = \
+    {PAPI_LD_INS, PAPI_SR_INS, PAPI_BR_INS, PAPI_TOT_CYC, PAPI_TOT_INS, PAPI_BR_MSP};
+  long long counters[PAPI_INST_EVENT_COUNT];
+  int retval;
+
+  if ((retval = PAPI_start_counters(events, PAPI_INST_EVENT_COUNT)) != PAPI_OK) {
+    fprintf(stderr, "PAPI failed to start counters: %s\n", PAPI_strerror(retval));
+    exit(1);
+  }
+
+  return;
+}
+
+void EndInstMonitor() {
+  long long counters[PAPI_INST_EVENT_COUNT];
+  int retval;
+
+  if ((retval = PAPI_stop_counters(counters, PAPI_INST_EVENT_COUNT)) != PAPI_OK) {
+    fprintf(stderr, "PAPI failed to read counters: %s\n", PAPI_strerror(retval));
+    exit(1);
+  }
+
+  std::cout << "Total load = " << counters[0] << "\n";
+  std::cout << "Total store = " << counters[1] << "\n";
+  std::cout << "Total branch = " << counters[2] << "\n";
+  std::cout << "Total cycle = " << counters[3] << "\n";
+  std::cout << "Total instruction = " << counters[4] << "\n";
+  std::cout << "Total branch misprediction = " << counters[5] << "\n";
+  
+  return;
+}
 
 /*
  * StartCacheMonitor() - Uses PAPI Library to start monitoring L1, L2, L3 cache misses  
  */
 void StartCacheMonitor() {
-  int events[PAPI_CACHE_EVENT_COUNT] = {PAPI_L1_TCM, PAPI_L2_TCM, PAPI_L3_TCM, PAPI_L1_TCA};
+  int events[PAPI_CACHE_EVENT_COUNT] = \
+    {PAPI_L1_TCM, PAPI_L2_TCM, PAPI_L3_TCM};//, PAPI_LD_INS, PAPI_SR_INS};
   long long counters[PAPI_CACHE_EVENT_COUNT];
   int retval;
 
@@ -27,7 +62,7 @@ void EndCacheMonitor() {
   long long counters[PAPI_CACHE_EVENT_COUNT];
   int retval;
 
-  if ((retval = PAPI_read_counters(counters, PAPI_CACHE_EVENT_COUNT)) != PAPI_OK) {
+  if ((retval = PAPI_stop_counters(counters, PAPI_CACHE_EVENT_COUNT)) != PAPI_OK) {
     fprintf(stderr, "PAPI failed to read counters: %s\n", PAPI_strerror(retval));
     exit(1);
   }
@@ -35,7 +70,8 @@ void EndCacheMonitor() {
   std::cout << "L1 miss = " << counters[0] << "\n";
   std::cout << "L2 miss = " << counters[1] << "\n";
   std::cout << "L3 miss = " << counters[2] << "\n";
-  std::cout << "Total cache ref = " << counters[3] << "\n";
+  //std::cout << "Total load = " << counters[3] << "\n";
+  //std::cout << "Total store = " << counters[4] << "\n";
 
   return;
 }

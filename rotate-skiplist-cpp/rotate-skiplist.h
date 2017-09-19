fa904f82233bac7bb66059f2836f6012c845faff
Ziqi Wang <ziqiw@mit.edu>
2017-09-19 01:51:36 -0400

    Adding template skeleton of the main skiplist class

--- rotate-skiplist-cpp/rotate-skiplist.h
+++ rotate-skiplist-cpp/rotate-skiplist.h
@@ -4,6 +4,28 @@
 #ifndef _H_ROTATE_SKIPLIST
 #define _H_ROTATE_SKIPLIST
 
+// This contains std::less and std::equal_to
+#include <functional>
+
+// Traditional C libraries 
+#include <ctsdlib>
+#include <cstdio>
+
+/*
+ * class RotateSkiplist - Main class of the skip list
+ *
+ * Note that for simplicity of coding, we contain all private classes used
+ * by the main Skiplist class in the main class. We avoid a policy-based design
+ * and do not templatize internal nodes in the skiplist as type parameters
+ *
+ * The skiplist is built based on partial ordering of keys, and therefore we 
+ * need at least one functor which is the key comparator. To make key comparison
+ * faster, a key equality checker is also required.
+ */
+template <typename KeyType, 
+          typename ValueType,
+          typename KeyLess = std::less,
+          typename KeyEq = std::equal_to>
 class RotateSkiplist {
 
 };

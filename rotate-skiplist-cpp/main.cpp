b0ab8fdce055fbee4675a80c060d64dfaaeb3618
Ziqi Wang <ziqiw@mit.edu>
2017-09-19 23:59:42 -0400

    Moving testing code into main.cpp

---
+++ rotate-skiplist-cpp/main.cpp
@@ -0,0 +1,15 @@
+
+#include "rotate-skiplist.h"
+
+// We do this for testing code
+using namespace rotate_skiplist;
+
+/*
+ * main() - The main testing function
+ */
+int main() {
+  RotateSkiplist<uint64_t, uint64_t> rsl{};
+  (void)rsl;
+  
+  return 0;
+}
--- rotate-skiplist-cpp/rotate-skiplist.cpp
+++ rotate-skiplist-cpp/rotate-skiplist.cpp
@@ -1,11 +1,2 @@
 
 #include "rotate-skiplist.h"
-
-/*
- * main() - The main testing function
- */
-int main() {
-  RotateSkiplist rsl{};
-  
-  return 0;
-}

e429da5ad40109827584f2c33cfe03739e67c31a
Ziqi Wang <ziqiw@mit.edu>
2017-09-20 00:34:39 -0400

    Defining global variables

--- rotate-skiplist-cpp/rotate-skiplist.cpp
+++ rotate-skiplist-cpp/rotate-skiplist.cpp
@@ -1,2 +1,8 @@
 
 #include "rotate-skiplist.h"
+
+// Three global variables defined within thread state object
+// which mains the global linked list of thread objects
+pthread_key_t ThreadState::thread_state_key;
+std::atomic<unsigned int> ThreadState::next_id;
+ThreadState *ThreadState::thread_state_head_p;

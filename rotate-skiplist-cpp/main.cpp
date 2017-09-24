06054bc276b93cefa8cc7efde80a63aff7f44be7
Ziqi Wang <ziqiw@mit.edu>
2017-09-23 22:36:53 -0400

    Adding test cases for class ThreadState

--- rotate-skiplist-cpp/main.cpp
+++ rotate-skiplist-cpp/main.cpp
@@ -10,6 +10,20 @@ using namespace rotate_skiplist;
 int main() {
   RotateSkiplist<uint64_t, uint64_t> rsl{};
   (void)rsl;
+
+  ThreadState::Init();
+  ThreadState *thread_state_p = ThreadState::EnterCritical();
+
+  assert(ThreadState::thread_state_head_p == thread_state_p);
+  assert(ThreadState::next_id.load() == 1U);
+  assert(thread_state_p->next_p == nullptr);
+  assert(thread_state_p->owned.test_and_set() == true);
+  // Has been registered
+  assert(pthread_getspecific(ThreadState::thread_state_key) == thread_state_p);
+
+  ThreadState::LeaveCritical(thread_state_p);
+
+  fprintf(stderr, "All tests have passed\n");
   
   return 0;
 }

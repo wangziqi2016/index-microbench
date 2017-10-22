c1e8439b842e56d614740f5ff8a999942881fcb4
Ziqi Wang <ziqiw@mit.edu>
2017-10-22 04:23:43 -0400

    Change the generic key interface to fit skiplist

--- indexkey.h
+++ indexkey.h
@@ -8,16 +8,31 @@
 template <std::size_t keySize>
 class GenericKey {
 public:
+  char data[keySize];
+public:
   inline void setFromString(std::string key) {
     memset(data, 0, keySize);
     strcpy(data, key.c_str());
   }
 
-  char data[keySize];
+  // Constructor - Fills it with 0x00
+  // This is for the skiplist to initialize an empty node
+  GenericKey(int) { memset(data, 0x00, keySize); }
+  GenericKey() { memset(data, 0x00, keySize); }
+  // Copy constructor
+  GenericKey(const GenericKey &other) { memcpy(data, other.data, keySize); }
+  inline GenericKey &operator=(const GenericKey &other) {  
+    memcpy(data, other.data, keySize);
+    return *this;
+  }
 
   inline bool operator<(const GenericKey<keySize> &other) { return strcmp(data, other.data) < 0; }
   inline bool operator>(const GenericKey<keySize> &other) { return strcmp(data, other.data) > 0; }
   inline bool operator==(const GenericKey<keySize> &other) { return strcmp(data, other.data) == 0; }
+  // Derived operators
+  inline bool operator!=(const GenericKey<keySize> &other) { return !(*this == other); }
+  inline bool operator<=(const GenericKey<keySize> &other) { return !(*this > other); }
+  inline bool operator>=(const GenericKey<keySize> &other) { return !(*this < other); }
 };
 
 template <std::size_t keySize>

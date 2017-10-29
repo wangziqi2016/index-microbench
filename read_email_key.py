a787371f9b4a37768b2ccc82977a35eaa450ceb8
Ziqi Wang <ziqiw@mit.edu>
2017-10-29 03:28:40 -0400

    Adding script to extract email keys

---
+++ read_email_key.py
@@ -0,0 +1,15 @@
+#
+# read_email_key.py - This file processes the email insert and workload file
+#                     and outputs a file to use the index
+#
+#
+# Run this file with the following arguments:
+#
+# python read_email_key.py [load file] [workload file]
+#
+
+import sys
+import os
+
+def read_load_file()
+
